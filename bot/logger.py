116e5b1ea7f5e02736a415159212b22d33896b6d
danidsnk <mdesinka@gmail.com>
2023-07-10 18:57:41 +0600

    move logger config to setup function

--- bot/logger.py
+++ bot/logger.py
@@ -2,52 +2,52 @@ import logging
 from logging.config import dictConfig
 import os
 
-LOGGING_CONFIG = {
-    'version': 1,
-    'disabled_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s'
-        },
-        'standard': {'format': '%(levelname)-10s - %(name)-15s : %(message)s'},
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'standard',
-        },
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': 'logs/infos.log',
-            'mode': 'w',
-            'formatter': 'verbose',
-            'encoding': 'utf-8',
-        },
-    },
-    'loggers': {
-        'bot': {
-            'handlers': ['console', 'file'],
-            'level': 'INFO',
-            'propagate': False
-        },
-        'discord': {
-            'handlers': ['console', 'file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-    },
-}
-
 
 def setup_logger(debug: bool):
     if not os.path.exists('logs'):
         os.mkdir('logs')
 
+    logging_config = {
+        'version': 1,
+        'disabled_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': '%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s'
+            },
+            'standard': {'format': '%(levelname)-10s - %(name)-15s : %(message)s'},
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'standard',
+            },
+            'file': {
+                'level': 'INFO',
+                'class': 'logging.FileHandler',
+                'filename': 'logs/infos.log',
+                'mode': 'w',
+                'formatter': 'verbose',
+                'encoding': 'utf-8',
+            },
+        },
+        'loggers': {
+            'bot': {
+                'handlers': ['console', 'file'],
+                'level': 'INFO',
+                'propagate': False
+            },
+            'discord': {
+                'handlers': ['console', 'file'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+        },
+    }
+
     if debug:
-        LOGGING_CONFIG['loggers']['bot']['level'] = 'DEBUG'
-    dictConfig(LOGGING_CONFIG)
+        logging_config['loggers']['bot']['level'] = 'DEBUG'
+    dictConfig(logging_config)
 
 
 def get_logger(name):
